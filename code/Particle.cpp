9ee3993a33e7a4feb4f1d5527bcd83c68571b14e
William Garcia-Cortes <headlessteve@gmail.com>
2023-05-10 10:54:38 -0700

    Renamed M_PI to PI

--- code/Particle.cpp
+++ code/Particle.cpp
@@ -4,7 +4,7 @@ Particle::Particle(RenderTarget& target, int numPoints, Vector2i mouseClickPosit
 {
     m_ttl = TTL;
     m_numPoints = numPoints;
-    m_radiansPerSec = M_PI * ((float)rand() / (RAND_MAX));
+    m_radiansPerSec = PI * ((float)rand() / (RAND_MAX));
     m_cartesianPlane.setCenter(0, 0);
     m_cartesianPlane.setSize(target.getSize().x, (-1.0) * target.getSize().y);
     m_centerCoordinate = target.mapPixelToCoords(mouseClickPosition, m_cartesianPlane);
@@ -27,8 +27,8 @@ Particle::Particle(RenderTarget& target, int numPoints, Vector2i mouseClickPosit
     m_color2 = rainbow[rand() % 6];
 
     // Create vertices of particle
-    float theta = (float)rand() / (RAND_MAX / (M_PI / 2));
-    float dTheta = 2 * M_PI / (numPoints - 1);
+    float theta = (float)rand() / (RAND_MAX / (PI / 2));
+    float dTheta = 2 * PI / (numPoints - 1);
 
     for (int j = 0; j < numPoints; j++)
     {
@@ -113,8 +113,8 @@ void Particle::unitTests()
     int score = 0;
 
     cout << "Testing RotationMatrix constructor...";
-    double theta = M_PI / 4.0;
-    RotationMatrix r(M_PI / 4);
+    double theta = PI / 4.0;
+    RotationMatrix r(PI / 4);
     if (r.getRows() == 2 && r.getCols() == 2 && almostEqual(r(0, 0), cos(theta))
         && almostEqual(r(0, 1), -sin(theta))
         && almostEqual(r(1, 0), sin(theta))
@@ -177,7 +177,7 @@ void Particle::unitTests()
 
     cout << "Applying one rotation of 90 degrees about the origin..." << endl;
     Matrix initialCoords = m_A;
-    rotate(M_PI / 2.0);
+    rotate(PI / 2.0);
     bool rotationPassed = true;
     for (int j = 0; j < initialCoords.getCols(); j++)
     {
